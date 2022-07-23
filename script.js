ad0f3e49c67ed463816ab24141da6dc7a908d257
Aydan <91035120+Aydanjb@users.noreply.github.com>
2022-07-23 15:41:51 -0400

    Revert "Update script.js"
    This reverts commit 5b2f258d453c18524abe445a87406a60072eff04.

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let playerWins = 0;
-let computerWins = 0;
-
 function getComputerChoice() {
     let x = Math.floor(Math.random() * 3);
     if (x == 0) {
@@ -15,57 +12,41 @@ function getComputerChoice() {
             }
 }
 
+let computerChoice = getComputerChoice();
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == "rock" && computerSelection == "paper") {
-        computerWins++;
-        return "You Lose this round! Paper beats Rock.";
+        return "You Lose! Paper beats Rock."
     }
         else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "scissors") {
-            playerWins++;
-            return "You Win this round! Rock beats Scissors.";
+            return "You Win! Rock beats Scissors."
         }
             else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "rock") {
-                return "Tie this round! Rock ties Rock.";
+                return "Tie! Rock ties Rock."
             }
                 else if (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "rock") {
-                    playerWins++;
-                    return "You Win this round! Paper beats Rock.";
+                    return "You Win! Paper beats Rock."
                 }
                     else if (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "scissors") {
-                        computerWins++;
-                        return "You Lose this round! Scissors beats Paper.";
+                        return "You Lose! Scissors beats Paper."
                     }
                         else if (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "paper") {
-                            return "Tie this round! Paper ties Paper.";
+                            return "Tie! Paper ties Paper."
                         }
                             else if (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "rock") {
-                                computerWins++;
-                                return "You Lose this round! Rock beats Scissors.";
+                                return "You Lose! Rock beats Scissors."
                             }
                                 else if (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "paper") {
-                                    playerWins++;
-                                    return "You win this round! Scissors beats Paper.";
+                                    return "You Win! Scissors beats Paper."
                                 }
                                     else if (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "scissors") {
-                                        return "Tie this round! Scissors ties Scissors.";
+                                        return "Tie! Scissors ties Scissors."
                                     }
+                                        else {
+                                            return "Please enter a valid selection."
+                                        }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let computerInput = getComputerChoice();
-        let userInput = prompt("Please Choose: Rock, Paper, or Scissors.");
-        console.log(playRound(userInput, computerInput))
-    }
-    if (playerWins > computerWins) {
-        console.log(`You win the game! Your Score: ${playerWins}, Computer: ${computerWins}`)
-    }
-        else if (computerWins > playerWins) {
-            console.log(`Computer wins the game! Your Score: ${playerWins}, Computer: ${computerWins}`)
-        }
-            else if (computerWins == playerWins) {
-                console.log(`Tie game! Your Score: ${playerWins}, Computer: ${computerWins}`)
-            }
-}
+let userInput = prompt("Please Choose: Rock, Paper, or Scissors.");
 
-game();
+console.log(playRound(userInput, computerChoice))
